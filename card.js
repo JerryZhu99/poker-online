90b2bb2b90e4728ad32309384d9e22ae9f2e1388
spoonsplz <modanhan@live.com>
2017-08-21 16:58:23 -0600

    made card class

---
+++ card.js
@@ -0,0 +1,57 @@
+_valuestr=["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+_suitstr=["♦","♣","♥","♠"]
+
+class Card{
+    constructor(value, suit){
+        this.value=value;
+        this.suit=suit;
+    }
+
+    get valuestr(){
+        return _valuestr[this.value];
+    }
+
+    get suitstr(){
+        return _suitstr[this.suit];
+    }
+
+    valueOf(){
+        return this.value*4+this.suit;
+    }
+}
+
+class Deck{
+    constructor(){
+        this.cards=new Array(52);
+        for(var i=0;i<13;i++)for(var j=0;j<4;j++)this.cards[i*4+j]=new Card(i,j);
+        this.shuffle();
+    }
+
+    shuffle(){
+        for(var i=51;i>=0;i--){
+            var j=Math.floor(Math.random()*(i+1));
+            var temp;
+            temp=this.cards[i];
+            this.cards[i]=this.cards[j];
+            this.cards[j]=temp;
+        }
+    }
+
+    sort(){
+        this.cards.sort(function(a,b){
+            return a.valueOf()-b.valueOf();
+        });
+    }
+
+    draw(){
+        return this.cards.pop();
+    }
+}
+
+// test
+
+d=new Deck();
+while(d.cards.length>0){
+    card = d.draw();
+    console.log(card.valuestr+ " "+card.suitstr+" "+card.valueOf());
+}
